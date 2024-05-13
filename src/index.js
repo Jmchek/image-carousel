3dd70efd5d6a4c41b869a15a869d134dc5a66f28
Joe Chek <jmchek@gmail.com>
2024-05-13 15:59:31 -0700

    improve style properties for wide-div

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import './style.css';
 
 const frameGrbbr = document.querySelector('.picture-frame');
+const nextBttn = document.querySelector('.next');
+const wideDivGrbbr = document.querySelector('.wide-div');
+let currentPosition = parseInt(nextBttn.style.right);
 
-console.log(frameGrbbr.getBoundingClientRect());
+// console.log(nextBttn.getBoundingClientRect());
 
-// frameGrbbr = frameGrbbr.
+nextBttn.addEventListener('click', x => {
+    // let leftPos = 0;
+
+    // if (leftPos == 0){
+    // wideDivGrbbr.style.left = leftPos - 750 + "px";
+    // leftPos -= 750;
+    // } else {
+    //     wideDivGrbbr.style.left += leftPos - 750 + "px";
+    // }
+    // wideDivGrbbr.style.left = leftPos - 750 + "px";
+    // leftPos -= 750;
+    wideDivGrbbr.style.setProperty('left', '-750px');
+});
