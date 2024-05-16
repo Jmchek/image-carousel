1218364b0260274d741a6963a1d515ce180b400a
Joe Chek <jmchek@gmail.com>
2024-05-15 17:26:20 -0700

    add icons to next and prev buttons, begin additon of icons to navigation dots

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import './style.css';
 
-const nextBttn = document.querySelector('.next');
-const wideDivGrbbr = document.querySelector('.wide-div');
-const prevBttn = document.querySelector('.prev');
 
-console.log(wideDivGrbbr.getBoundingClientRect());
 
 function imageCarousel() {
+  const nextBttn = document.querySelector(".next");
+  const wideDivGrbbr = document.querySelector(".wide-div");
+  const prevBttn = document.querySelector(".prev");
+  const navBubble1 = document.querySelector(".nav-dot-1");
+  const navBubble2 = document.querySelector(".nav-dot-2");
+  const navBubble3 = document.querySelector(".nav-dot-3");
+  const navBubble4 = document.querySelector(".nav-dot-4");
+  const navBubble5 = document.querySelector(".nav-dot-5");
   let leftPos = 0;
 
   nextBttn.addEventListener("click", (x) => {
@@ -14,7 +18,7 @@ function imageCarousel() {
       leftPos = 750;
     }
 
-    wideDivGrbbr.style.setProperty("left", (leftPos - 750) + "px");
+    wideDivGrbbr.style.setProperty("left", leftPos - 750 + "px");
     leftPos -= 750;
   });
 
@@ -23,8 +27,12 @@ function imageCarousel() {
       leftPos = -3750;
     }
 
-    wideDivGrbbr.style.setProperty("left", (leftPos + 750) + "px");
+    wideDivGrbbr.style.setProperty("left", leftPos + 750 + "px");
     leftPos += 750;
+  });
+
+  navBubble1.addEventListener("click", (x) => {
+    wideDivGrbbr.style.setProperty("left", 0 + "px");
   });
 }
 
